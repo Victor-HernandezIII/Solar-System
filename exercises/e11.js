26d26f169ddef1112692a4b121b6ca8f519f8669
Victor Hernandez <garyoakiii@yahoo.com>
2024-08-19 17:37:55 -0500

    Pass Test 11

--- exercises/e11.js
+++ exercises/e11.js
@@ -7,6 +7,11 @@ import { data } from "../data/data";
 
 export function lowMoonsPlanets(data) {
   // Your code goes here...
+   const lowMoons = data.planets
+        .filter(planet => {return planet.moons === undefined || planet.moons.length < 10})
+        .map(planet => {return planet.name});
+
+  return lowMoons;
 }
 
 // === TEST YOURSELF ===
